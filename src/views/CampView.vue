c28741a8ff3b5f2fbd9e3775aeda0ff8a3eb0cb5
tiffany1029tw <tiffany1029tw@gmail.com>
2024-01-18 16:44:04 +0800

    營地導覽頁面編輯

--- src/views/CampView.vue
+++ src/views/CampView.vue
@@ -6,7 +6,19 @@ export default {};
 </script>
 
 <template>
-  <div>This is 營地導覽 page</div>
+  <div class="guide-banner">
+    <h1 class="guide-title">營地導覽</h1>
+    <h2 class="guide-subtitle">我們一起看看營區的美吧！</h2>
+  </div>
+
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+  .guide-banner {
+    height: 400px;
+    width: 100vw;
+    background-color: $blue-1;
+    color: $blue-4;
+  }
+
+</style>
