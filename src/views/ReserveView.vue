89d5c68f2cc4684fd308872e2270d29d9342ffce
Orilla_Lin <142280255+rillala@users.noreply.github.com>
2024-01-18 23:19:43 +0800

    進度條元件創建

--- src/views/ReserveView.vue
+++ src/views/ReserveView.vue
@@ -1,12 +1,31 @@
 <script>
 // 引入函式庫
 import axios from 'axios';
+import { RouterLink, RouterView } from 'vue-router';
+// import progressBar from '@/components/reserve/bannerStep.vue';
+import equipmentPage from '@/views/reserve/EquipmentView.vue';
 
-export default {};
+export default {
+  components:{
+    // progressBar,
+    equipmentPage,
+  }
+};
 </script>
 
 <template>
-  <div>This is RESERVE page</div>
+  <section class="equipment">
+  <!--暫時把租借設備頁面導進來這頁開發-->
+  <equipmentPage />
+  <!-- <progressBar :activeDiv="1"/> -->
+
+ 
+
+
+
+  </section>
+  
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+</style>
---
+++ src/views/reserve/EquipmentView.vue
@@ -0,0 +1,24 @@
+<script>
+// 引入函式庫
+import axios from 'axios';
+import { RouterLink, RouterView } from 'vue-router';
+import progressBar from '@/components/reserve/bannerStep.vue';
+
+export default {
+  components:{
+    progressBar,
+  }
+};
+</script>
+
+<template>
+  <section class="equipment">
+  <progressBar :activeDiv="2"/>
+
+
+  </section>
+  
+</template>
+
+<style lang="scss" scoped>
+</style>
