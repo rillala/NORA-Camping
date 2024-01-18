0b5d61b3b5ff27d3e14442f4a809a013a93b230a
HsiaoYingChen <sandra401120422@gmail.com>
2024-01-18 23:27:00 +0800

    memberLogin_template

--- src/components/memberLogin.vue
+++ src/components/memberLogin.vue
@@ -1,45 +1,42 @@
 <template>
   <div class="lightbox">
-    <div v-if="isOpen" >
+    <div v-if="isOpen" class="lightbox-content" @click="closeLightbox">
       <!-- 切換表單的按鈕 -->
-      <div>
-        <div class="tab-add" @click="showRegisterForm = true" :class="{ active: showRegisterForm }">註冊會員</div>
-        <div class="tab-register" @click="showRegisterForm = false" :class="{ active: !showRegisterForm }">登入</div>
+      <div class="tab-container" @click.stop>
+        <button class="tab-add" @click="showRegisterForm = true" :class="{ active: showRegisterForm }">註冊會員</button>
+        <button class="tab-register" @click="showRegisterForm = false" :class="{ active: !showRegisterForm }">會員登入</button>
       </div>
 
       <!-- 註冊頁面 -->
-      <div v-if="showRegisterForm" @click="closeLightbox" >
-        <div class="lightbox-content">
-          <input type="text" placeholder="請輸入信箱" v-model="user_add.account">  
-          <input type="password" placeholder="請輸入密碼" v-model="user_add.pwd">  
-          <input type="password" placeholder="再次輸入密碼" v-model="user_add.pwdConfirmation">
+      <form v-if="showRegisterForm">
+          <input type="text" placeholder="請輸入信箱" v-model="user_add.account"><br>
+          <input type="password" placeholder="請輸入密碼" v-model="user_add.pwd"><br>
+          <input type="password" placeholder="再次輸入密碼" v-model="user_add.pwdConfirmation"><br>
 
-          <div class="login_news">
+          <div class="login-news">
             <input type="checkbox" v-model="user_add.receiveNews" id="receiveNews">
             <label for="receiveNews">
               <p>我願意接收野良的最新消息、優惠相關訊息</p>
             </label>
           </div>
 
-          <div class="login_news">
+          <div class="login-news">
             <input type="checkbox" v-model="user_add.agreeTerms" id="agreeTerms">
             <label for="agreeTerms">
               <p>我同意網站服務條款及隱私權政策</p>
             </label>
           </div>
 
-          <button type="submit" @click="joinAjax">立即加入</button>
-        </div>
-      </div>
+          <button type="submit" class="main-btn">立即加入</button>
+        </form>
 
       <!-- 登入頁面 -->
-      <div v-else @click="closeLightbox">
-        <input type="text" placeholder="請輸入信箱" v-model="user_enter.account">
-        <button type="submit" @click="loginAjax">會員登入</button>
-        <a @click="forgetPsw">忘記密碼？</a>
-
-        <button @click="loginWithGoogle">以Google登入</button>
-        <button @click="loginWithFacebook">以Facebook登入</button>
+      <div v-else>
+        <input type="text" placeholder="請輸入信箱" v-model="user_enter.account"><br>
+        <input type="password" placeholder="請輸入密碼" v-model="user_enter.pwd"><br>
+        <a class="forget-psw">忘記密碼？</a><br>
+        <router-link to="/" class="main-btn">會員登入</router-link><br>
+        <button type=button class="sub-btn">以Google登入</button>
       </div>
     </div>
   </div>
@@ -47,7 +44,8 @@
 
 <script>
 export default {
-props:['isOpen'],
+  props:['isOpen'],
+
   data() {
     return {
       showRegisterForm: true,
@@ -65,38 +63,17 @@ props:['isOpen'],
     };
   },
   methods: {
-    joinAjax() {
-      // 處理註冊表單的提交
-      this.closeLightbox();
-    },
-    loginAjax() {
-      // 處理登入表單的提交
-      this.closeLightbox();
-    },
-    forgetPsw() {
-      // 處理忘記密碼的操作
-      this.closeLightbox();
-    },
-    loginWithGoogle() {
-      // 處理以Google登入的操作
-      this.closeLightbox();
-    },
-    loginWithFacebook() {
-      // 處理以Facebook登入的操作
-      this.closeLightbox();
-    },
     closeLightbox() {
-      // 關閉燈箱
-      this.showRegisterForm = false;
+      if (this.isOpen) {
+        this.isOpen = false;
+      }
     }
   }
 };
 </script>
 
 <style lang="scss" scoped>
-
-/* 這裡可以添加樣式，視情況而定 */
-
+@import '@/assets/sass/main.scss';
 .lightbox {
   position: fixed;
   top: 0;
@@ -107,12 +84,80 @@ props:['isOpen'],
   display: flex;
   align-items: center;
   justify-content: center;
-  cursor: pointer;
 }
 
 .lightbox-content {
   background: #fff;
+  height: 400px;
+  width: 450px;
   padding: 20px;
   cursor: auto;
+}
+
+.tab-container{
+  display: flex;
+  justify-content: center; 
+}
+
+.tab-add,
+.tab-register {
+  background-color: transparent; 
+  border: none; 
+  text-decoration: none;
+  font-size: 32px;
+  padding: 0px 20px;
+  margin: 10px 15px;
+
+}
+.tab-add.active, .tab-register.active {
+  border-bottom: 2px solid #3EB3C2
+}
+
+p{
+  font-size: 16px;
+}
+
+input {
+  padding: 10px 90px 10px 0px;
+  font-size: 20px;
+  border: none; 
+  border-bottom: 1px solid #000; 
+  outline: none;
+  background-color: transparent; 
+}
+
+.login-news{
+  display: flex;
+  margin: 10px 0px;
+  padding-left:20px
+}
+
+.main-btn{
+background-color:#3EB3C2;
+color: #fff;
+font-size: 24px;
+border-radius: 50px;
+border: none; 
+padding: 10px 25px;
+margin-top:10px;
+text-decoration:none;
+
+}
+
+.sub-btn{
+background-color:#BAE6EC;
+color:#000;
+font-size: 24px;
+border-radius: 50px;
+border: none; 
+padding: 10px 25px;
+margin-top:20px;
+}
+
+.forget-psw{
+  display: inline-block;
+  text-align: end;
+  margin-top: 20px;
+  font-size: 16px;
 }
 </style>
