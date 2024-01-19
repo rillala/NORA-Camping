23c76112da958abbe03ec46e7fbb2d706404f285
Orilla_Lin <142280255+rillala@users.noreply.github.com>
2024-01-19 18:18:53 +0800

    修改設備頁面中ing

--- src/views/reserve/EquipmentView.vue
+++ src/views/reserve/EquipmentView.vue
@@ -59,11 +59,29 @@ export default {
 <template>
   <section class="equipment">
     <progressBar :activeDiv="2" />
+    <div class="intro-container">
+      <h4 class="title dark">裝備租借</h4>
+      <ul class="intro">
+        <li>
+          <p class="dark">裝備在租借前會進行檢查和清潔。</p>
+        </li>
+        <li>
+          <p class="dark">
+            租借時需於現場支付押金，歸還裝備時若無損壞將全數退還。
+          </p>
+        </li>
+        <li>
+          <p class="dark">租借天數為露營總天數。</p>
+        </li>
+      </ul>
+    </div>
   </section>
+
+  <!--以下都是測試用, 不是正式code-->
   <button @click="goToNextStep">要按過這個才可以進入下一步驟:3確認畫面</button>
-  <input type="text" v-model="search" @input="filterHandle" />
 
   <!--下面抓API測試-->
+  <input type="text" v-model="search" @input="filterHandle" />
   <div class="card-list">
     <div v-for="card in displayData" class="card">
       <div class="title">{{ card.title }}</div>
@@ -74,6 +92,8 @@ export default {
 </template>
 
 <style lang="scss" scoped>
+@import '@/assets/sass/page/equipment.scss';
+
 .card-list {
   display: flex;
   flex-wrap: wrap;
