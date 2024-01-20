3d612b0080d7597d6f8ed69108db56e42c590338
Orilla_Lin <142280255+rillala@users.noreply.github.com>
2024-01-20 13:35:33 +0800

    +loading 畫面

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,56 @@ import { RouterLink, RouterView } from 'vue-router';
 import headerCom from '@/components/header.vue';
 import footerCom from '@/components/footer.vue';
 import backToTop from '@/components/button/backToTop.vue';
+
+// 以下做loading畫面用
+import { ref, onMounted, onUnmounted } from 'vue';
+import { useRouter } from 'vue-router';
+
+const isLoading = ref(false);
+const router = useRouter();
+
+const setLoading = loading => {
+  isLoading.value = loading;
+};
+
+const beforeEach = () => {
+  setLoading(true);
+};
+
+const afterEach = () => {
+  setLoading(false);
+};
+
+onMounted(() => {
+  router.beforeEach(beforeEach);
+  router.afterEach(afterEach);
+});
+
+onUnmounted(() => {
+  // 在這裡，您需要正確移除守衛
+  router.beforeEach().unbind(beforeEach);
+  router.afterEach().unbind(afterEach);
+});
 </script>
 
 <template>
-  <headerCom />
-  <div id="app-content-wrap">
-    <RouterView />
-    <div id="app-sidebar">
-      <backToTop id="app-top-btn" />
+  <section>
+    <headerCom />
+    <div id="app-content-wrap">
+      <div>
+        <!-- 加載畫面 -->
+        <div v-if="isLoading" id="app-loading">Loading...</div>
+
+        <!-- 路由視圖 -->
+        <RouterView v-else />
+      </div>
+
+      <div id="app-sidebar">
+        <backToTop id="app-top-btn" />
+      </div>
     </div>
-  </div>
-  <footerCom />
+    <footerCom />
+  </section>
 </template>
 
 <style lang="scss">
@@ -38,5 +77,12 @@ body {
 #app-top-btn {
   position: sticky;
   top: 85svh;
+}
+
+// loading 畫面,測試用
+#app-loading {
+  width: 100%;
+  height: 100px;
+  background: red;
 }
 </style>
