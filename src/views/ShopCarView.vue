44a55cb88630fad10ca508069cf14c64cab7ebed
victor6835 <victor6835@gmail.com>
2024-01-30 10:40:07 +0800

    購物車頁面RWD

--- src/views/ShopCarView.vue
+++ src/views/ShopCarView.vue
@@ -4,6 +4,7 @@ import axios from 'axios';
 import { useCartStore } from '@/stores/cartStore';
 import { mapState, mapActions } from 'pinia';
 import bannerStepShop from '@/components/shop/bannerStep-shop.vue';
+import actionBtn from '@/components/button/actionBtn.vue';
 
 
 export default {
@@ -16,11 +17,17 @@ export default {
   computed: {
     ...mapState(useCartStore, ['cartList'])
   },
+  watch:{
+  // cartList(newVal){
+  //   alert('success');
+  //   localStorage.setItem('cartList',newVal)
+  // },
+  },
   created() {
     // const productStore = useProductStore();
     // productStore.axiosGetData();
   },
-  components: { bannerStepShop }
+  components: { bannerStepShop, actionBtn }
 };
 </script>
 
@@ -33,42 +40,50 @@ export default {
         <table class="cart-list" v-else>
           <tbody>
             <tr>
-              <th>圖片</th>
+              <th style="border-radius: 15px 0 0 0"></th>
               <th>商品</th>
-              <th class="cart-selectColor-desktop">顏色</th>
-              <th class="cart-selectSize-desktop">尺寸</th>
+              <th class="cart-selectColor-desktop">color</th>
+              <th class="cart-selectSize-desktop">size</th>
               <th>單價</th>
               <th>數量</th>
               <th>小計</th>
-              <th></th>
+              <th style="border-radius: 0 15px 0 0"></th>
             </tr>
             <tr v-for="item in cartList.carts" :key="item.id">
-              <td >
+              <td>
                 <img :src="item.product.images" alt="">
               </td>
-              <td >
-                <p>{{ item.product.title }}</p>
+              <td class="select-content">
+                <p class="footerp">{{ item.product.title }}</p>
+                <div class="select">
+                  <p class="select-item">顏色</p>
+                  <p class="select-item">尺寸</p>
+                </div>
               </td>
-              <td >顏色</td>
-              <td >尺寸</td>
-              <td >
+              <td>
                 <p>{{ item.product.price }}</p>
               </td>
-              <td >
+              <td>
                 <select name="" id="" :value="item.qty" @change="(event) => setCartQty(item.id, event)">
                   <option :value="i" v-for="i in 20" :key="i">{{ i }}</option>
                 </select>
               </td>
-              <td >
-                $ {{ item.subtatal }}
+              <td>
+                <p>$ {{ item.subtotal }}</p>
               </td>
-              <td >
-                <img src="/src/assets/image/shop/icons/cancel.svg" @click.prevent="removeCartItem(item.id)" alt="cancel">
+              <td>
+                <img class="cart-cancel" src="/src/assets/image/shop/icons/cancel.svg" @click.prevent="removeCartItem(item.id)" alt="cancel">
               </td>
             </tr>
+            
           </tbody>
         </table>
-        <td>總金額 NT$ {{ cartList.total }}</td>
+        <p class="cart-total">總金額 NT$ {{ cartList.total }}</p>
+        <div class="cart-buttons">
+          <router-link to="shop"><actionBtn class="cart-keepShoping" :content="'繼續購物'"></actionBtn></router-link>
+          
+          <actionBtn class="cart-placeOrder" :content="'前往結帳'"></actionBtn>
+        </div>
       </div>
     </div>
   </div>
@@ -83,32 +98,114 @@ export default {
     max-width: 1200px;
     height: 100%;
     margin: auto;
-    padding: 20px;
+    padding: 20px 4px;
 
     .cart-background {
-      padding: 8px;
+      padding: 10px 8px;
       background-color: $blue-2;
       border-radius: 20px;
 
       .cart-list {
+        width: 100%;
         background-color: #fff;
-        border-radius: 15px
+        border-radius: 15px;  
+      }
+      .cart-total{
+        margin: 20px 0;
+        text-align: center;
+      }
+
+      tr {
+        box-shadow: 0 1px 5px rgba(0, 0, 0, 0.2);
+      }
+
+      tr:nth-child(1) {
+        border-radius: 20px 20px 0 0;
+        box-shadow: 0 1px 5px rgba(0, 0, 0, 0.2);
+      }
+      tr:last-child {
+        border-radius: 0 0 15px 15px;
+      }
+
+      th {
+        width: 25vw;
+        line-height: 200%;
+        background-color: $light-gray;
+
+      }
+
+      td {
+        text-align: center;
+        vertical-align: middle;
+        padding:4px 2px;
       }
     }
-
-    // .cart-content{
-    //   padding: 40px;
-    //   border-radius: 30px
-    // }
-    // .cart-list{
-    //   width: 100%;
-    //   background-color: #fff;
-    //   border-radius: 40px
-
-    // }
   }
 }
-
+.cart-buttons{
+  display: flex;
+  justify-content: center;
+  gap: 20px;
+}
+.cart-keepShoping{
+  background-color: $blue-1;
+  border: none;
+}
 img {
   width: 30px;
-}</style>
+  vertical-align: middle;
+}
+@include desktop{
+img{
+  width: 50px;
+}
+}
+.cart-cancel{
+  width: 15px;
+  height: 15px;
+}
+@include desktop{
+  .cart-cancel{width: 20px;
+  height: 20px;}
+    .select {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .select-content {
+    display: flex;
+    margin-top: 6px;
+  }
+
+}
+
+/* select color size */
+.select-title {
+  display: flex;
+}
+.select {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.select p {
+  background-color: rgb(127, 255, 148);
+  border-radius: 5px;
+  padding: 3px 5px;
+  font-size: 14px;
+  width: 50px;
+}
+
+.select p:nth-child(1) {
+  margin-right: 5px;
+}
+
+.cart-selectColor-desktop {
+  display: none;
+}
+
+.cart-selectSize-desktop {
+  display: none;
+}
+</style>
