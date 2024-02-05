9423ad3fe7452553d4e21a409fb5f020ffefc6cc
nan-sansan <a85124236@gmail.com>
2024-02-05 13:43:59 +0800

    動畫組件新增

---
+++ src/components/about/animation.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+
+</script>
+
+<template>
+
+    <div class="picWrap">
+        HEllo
+    </div>
+
+</template>
+
+<style lang="scss" scopedS>
+.picWrap{
+    background-color: #f0f;
+    width: 100%;
+    height: 300px;
+}
+
+</style>
